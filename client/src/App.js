51a7cc953909626ddf45c89dd632f92b7e912716
Allison Reynolds <allisonreynolds@Allisons-MacBook-Air.local>
2021-04-11 15:24:17 -0500

    sizing

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,6 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
-  //https://stackoverflow.com/questions/17756649/disable-the-horizontal-scroll
-  let scrollEventHandler = function () {
-    window.scroll(0, window.pageYOffset);
-  };
-  window.addEventListener("scroll", scrollEventHandler, false);
-
   return (
     <div className="App">
       <Switch>
