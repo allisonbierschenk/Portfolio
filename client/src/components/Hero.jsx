a7091911e9ad5e8f0e8ba00bcafe7a940c728e6a
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-13 10:32:31 -0500

    twinkling stars

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
-import ParticlesBg from "particles-bg";
-
+//https://www.sliderrevolution.com/resources/css-animated-background/
 import "../assets/Hero.css";
 
 export default function Hero() {
   return (
     <div id="hero" className="hero">
-      <ParticlesBg color="#f72585" type="cobweb" bg={true} />
-      <div className="socia-media">
+      <div className="background-container">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+      </div>
+
+      {/* <div className="socia-media">
         {/* <img className="icon" src="https://i.imgur.com/OUTCgGe.png" />
         <img className="icon" src="https://i.imgur.com/MiKSPkq.png" /> */}
-      </div>
-      {/* <div className="construction"> ⊝This site is under construction⊝</div> */}
+      {/* </div> */}
       <div className="hello">Welcome! I'm Allison Bierschenk</div>
-      <div className="container">
-        <div className="image-hero">
-          <img
-            src="https://i.imgur.com/3UEODg1.jpg?1"
-            className="img"
-            alt="allison-bierschenk"
-          ></img>
-        </div>
+      <div className="image-hero">
+        <img
+          src="https://i.imgur.com/3UEODg1.jpg?1"
+          className="my-pic"
+          alt="allison-bierschenk"
+        ></img>
       </div>
     </div>
   );
