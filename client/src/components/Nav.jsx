a65ca650bf8d443842e533f8dd9c42366078a0ff
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-04-24 20:06:55 -0500

    update resume

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -32,7 +32,7 @@ function Nav(props) {
           <h1>Get In Touch</h1>
         </Link>
         <a
-          href="https://drive.google.com/file/d/1EQIiW-R-AqbgLK3esL_H3H-189mSESso/view?usp=sharing"
+          href="https://drive.google.com/file/d/1W1UVDii6IwO0kUcWqvw5Tpls1AwCfEqP/view?usp=sharing"
           target="_blank"
         >
           <h1 className="resume">resume</h1>
