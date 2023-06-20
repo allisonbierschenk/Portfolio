91ef1cf768b72613eae6d018581cc7c337f1bfed
ALLISON <allisonreynoldsc@gmail.com>
2023-06-20 13:24:21 -0500

    fix warnings

--- client/src/components/PortfolioPiecesContainer.jsx
+++ client/src/components/PortfolioPiecesContainer.jsx
@@ -7,7 +7,7 @@ export default function PortfolioPiecesContainer() {
       <div className="projects-intro">Coding projects as the Developer...</div>
       <div className="projects-container">
         <div className="each-project-container">
-          <img className="image" src="https://i.imgur.com/PMJR5xR.png?1" />
+          <img className="image" alt="demandjump homepage" src="https://i.imgur.com/PMJR5xR.png?1" />
           <div className="details">
             <div>
               <div className="title">DemandJump</div>
@@ -17,19 +17,20 @@ export default function PortfolioPiecesContainer() {
             </div>
           </div>
           <div className="buttons">
-            <a href="https://beta.app.demandjump.com/home?pid=51" target="_blank">
+            <a href="https://beta.app.demandjump.com/home?pid=51" target="_blank" rel="noreferrer">
               <button className="button">See My Site</button>
             </a>
             <a
               href="https://github.com/allisonbierschenk?tab=overview "
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Contributions</button>
             </a>
             </div>
           </div>
         <div className="each-project-container">
-          <img className="image" src="https://i.imgur.com/oBIeaCe.jpg?1" />
+          <img className="image" alt="travel like a local blog" src="https://i.imgur.com/oBIeaCe.jpg?1" />
           <div className="details">
             <div>
               <div className="title">Travel Like A Local</div>
@@ -41,19 +42,20 @@ export default function PortfolioPiecesContainer() {
             </div>
           </div>
           <div className="buttons">
-            <a href="https://travel-like-a-local.netlify.app" target="_blank">
+            <a href="https://travel-like-a-local.netlify.app" target="_blank" rel="noreferrer">
               <button className="button">See My Site</button>
             </a>
             <a
               href="https://github.com/allisonbierschenk/Travel-Like-A-Local"
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Code</button>
             </a>
           </div>
         </div>
         <div className="each-project-container">
-          <img className="image" src="https://i.imgur.com/7GZ7Xb4.jpg?1" />
+          <img className="image" alt="driving app" src="https://i.imgur.com/7GZ7Xb4.jpg?1" />
           <div className="details">
             <div>
               <div className="title">Sunday Driver</div>
@@ -64,19 +66,20 @@ export default function PortfolioPiecesContainer() {
             </div>
           </div>
           <div className="buttons">
-            <a href="https://sundaydriver.netlify.app/" target="_blank">
+            <a href="https://sundaydriver.netlify.app/" target="_blank" rel="noreferrer">
               <button className="button">See My Site</button>
             </a>
             <a
               href="https://github.com/allisonbierschenk/car-app"
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Code</button>
             </a>
           </div>
         </div>
         <div className="each-project-container">
-          <img className="image" src="https://i.imgur.com/ctYDaTC.jpg" />
+          <img className="image" alt="workout tracker" src="https://i.imgur.com/ctYDaTC.jpg" />
           <div className="details">
             <div>
               <div className="title">WorkOut Tracker App</div>
@@ -88,19 +91,20 @@ export default function PortfolioPiecesContainer() {
             </div>
           </div>
           <div className="buttons">
-            <a href="https://whiteboard-workout.netlify.app/" target="_blank">
+            <a href="https://whiteboard-workout.netlify.app/" target="_blank" rel="noreferrer">
               <button className="button">See My Site</button>
             </a>
             <a
               href="https://github.com/allisonbierschenk/Work_it_out"
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Code</button>
             </a>
           </div>
         </div>
         <div className="each-project-container">
-          <img className="image" src="https://i.imgur.com/Zukotas.jpg" />
+          <img className="image" alt="workout" src="https://i.imgur.com/Zukotas.jpg" />
           <div className="details">
             <div>
               <div className="title">GoodNews</div>
@@ -113,12 +117,14 @@ export default function PortfolioPiecesContainer() {
             <a
               href="https://allisonbierschenk.github.io/good-news/"
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Site</button>
             </a>
             <a
               href="https://github.com/allisonbierschenk/good-news"
               target="_blank"
+              rel="noreferrer"
             >
               <button className="button">See My Code</button>
             </a>
