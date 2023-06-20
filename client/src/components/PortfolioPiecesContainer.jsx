e1da4180db1ebeb57341b811074dc714fa226b2a
ALLISON <allisonreynoldsc@gmail.com>
2023-06-20 13:19:59 -0500

    spacing

--- client/src/components/PortfolioPiecesContainer.jsx
+++ client/src/components/PortfolioPiecesContainer.jsx
@@ -6,7 +6,7 @@ export default function PortfolioPiecesContainer() {
     <div id="projects-dev" className="intro">
       <div className="projects-intro">Coding projects as the Developer...</div>
       <div className="projects-container">
-        <div className="showcase">
+        <div className="each-project-container">
           <img className="image" src="https://i.imgur.com/PMJR5xR.png?1" />
           <div className="details">
             <div>
