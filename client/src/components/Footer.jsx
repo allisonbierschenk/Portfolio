ef83c86ccbb7e7e3fe9a7a82ad9941654b3cff8e
ALLISON <allisonreynoldsc@gmail.com>
2023-06-28 12:22:47 -0500

    lnkedin

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "../assets/Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
   return (
     <div className="footer">
-      {/* <div className="logo-footer">Logo Placeholder</div> */}
-      <div className="attribution">
+      <div className="linkedin">
+        <a
+          href="https://www.linkedin.com/in/allison-bierschenk/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon" />
+        </a>
+        </div>
+        <div className="attribution">
         Icons made by{" "}
         <a
           href="https://www.flaticon.com/authors/pixel-perfect"
@@ -17,7 +27,7 @@ export default function Footer() {
         <a href="https://www.flaticon.com/" title="Flaticon">
           www.flaticon.com
         </a>
-      </div>
+        </div>
     </div>
   );
 }
